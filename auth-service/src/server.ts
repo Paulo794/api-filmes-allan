44cc02b74eb331951c5b58c534f8b5fb89d8a362
Paulo Henrique <antunespaulohenrique721@gmail.com>
2026-09-24 18:01:42 -0300

    feat(auth): adicionei o Swagger

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 dotenv.config();
 
@@ -9,6 +12,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Documentação Swagger
+const swaggerPath = path.join(process.cwd(), 'src', 'docs', 'openapi.yaml');
+const swaggerDocument = YAML.load(swaggerPath);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Rotas
 app.get('/health', (req, res) => res.status(200).json({ status: 'ok' }));
